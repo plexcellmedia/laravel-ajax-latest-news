f28b1d2b2e6483c84471e6f00385d4dbb2f93098
plexcellmedia <niko@plexcellmedia.com>
2015-11-23 23:05:28 +0200

    Create ExampleController.php

---
+++ controllers/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ExampleController extends Controller {
+
+  protected function home(){
+  
+    // Load news on page load
+		$news = DB::table('news')->orderBy('ts', 'desc')->paginate(5);
+		
+		// If ajax request, return news in JSON array
+		if (Request::ajax()) {
+			return Response::json(View::make('news', array('news' => $news))->render());
+		}
+		
+		return View::make('home', array('news' => $news));
+	}
+	
+}
