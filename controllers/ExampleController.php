a4c5dcfd3249924049a2a5e667f05fd9924190c2
plexcellmedia <niko@plexcellmedia.com>
2015-11-23 23:24:06 +0200

    Update ExampleController.php

--- controllers/ExampleController.php
+++ controllers/ExampleController.php
@@ -1,18 +1,22 @@
 <?php
 
 class ExampleController extends Controller {
-
-  protected function home(){
-  
-    // Load news on page load
+	
+	protected function home() {
+		
+		// Load news on page load
 		$news = DB::table('news')->orderBy('ts', 'desc')->paginate(5);
 		
 		// If ajax request, return news in JSON array
 		if (Request::ajax()) {
-			return Response::json(View::make('news', array('news' => $news))->render());
+			return Response::json(View::make('news', array(
+				'news' => $news
+			))->render());
 		}
 		
-		return View::make('home', array('news' => $news));
+		return View::make('home', array(
+			'news' => $news
+		));
 	}
 	
 }
