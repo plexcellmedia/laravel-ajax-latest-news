324ddc6a4d7cbd2dca6ae324ef2d63625eab74e2
plexcellmedia <niko@plexcellmedia.com>
2015-11-23 23:08:05 +0200

    Create home.blade.php

---
+++ views/home.blade.php
@@ -0,0 +1,51 @@
+@extends('layout')
+
+@section('content')
+	<div class="page-header">
+		<h3>Latest News</h3>
+	</div>
+
+	<div class="news">
+		@include('layouts.news')
+	</div>
+@stop
+
+@section('scripts')
+
+	{{ HTML::script(url('assets/js/plugins.min.js')) }}
+	{{ HTML::script(url('assets/js/bootslider.min.js')) }}
+
+	<script type="text/javascript">
+
+		var cache = [];
+
+		function getPosts(page) {
+
+			for(var i = 0; i <= cache.length - 1; i++){
+				if(cache[i]['page'] == page){
+					$('.news').html(cache[i]['data']);
+					return;
+				}
+			}
+			
+			$.ajax({
+				url : '?page=' + page,
+				dataType: 'json',
+			}).done(function (data) {
+				$('.news').html(data);
+				cache.push({page : page, data : data});
+			}).fail(function () {
+				alert('News could not be loaded.');
+			});
+
+
+		}
+
+		$(document).on('click', '.pagination a', function (e) {
+			getPosts($(this).attr('href').split('page=')[1]);
+			e.preventDefault();
+		});
+
+	</script>
+
+@stop
