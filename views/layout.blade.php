1f7ca827fbc9333c46d711820336db00af35b960
plexcellmedia <niko@plexcellmedia.com>
2015-11-23 23:09:55 +0200

    Create layout.blade.php

---
+++ views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<title>Laravel 4.2 Ajax Latest News</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+
+	</head>
+	<body>
+
+		<div class="container">
+			@yield('content')
+		</div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		
+		@yield('scripts')
+
+	</body>
+</html>
